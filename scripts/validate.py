98d82297768790fa9a6946b5409dd3760bc2587a
Kenneth Hanson <khanson679@gmail.com>
2015-10-19 14:27:28 +0900

    Added validation script.
    For now, just checks keywords.

---
+++ scripts/validate.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2.7
+
+"""
+Validate contents of a bibliography file with respect to Lingbib conventions.
+
+For now, just checks if keywords are on master list.
+"""
+
+from __future__ import print_function
+import sys
+import os
+import glob
+from pprint import pprint
+
+try:
+    import bibtexparser as btp
+except ImportError as e:
+    print(e)
+    print("Please install 'bibtexparser' with `pip install bibtexparser`")
+    sys.exit(1)
+
+
+KEYWORD_FILE = "database_src/keywords.txt"
+BIB_FILENAMES = sys.argv[1:]
+
+
+#
+# sanity check
+#
+
+if not os.path.isfile(KEYWORD_FILE):
+    print("Can't find '{}'".format(KEYWORD_FILE))
+    print("Are you in the root of Lingbib?")
+    sys.exit(1)
+
+if not BIB_FILENAMES:
+    print("Need at least one `.bib` file to check.")
+    sys.exit(1)
+
+
+#
+# check for undocumented keywords
+#
+
+master_kw_list = set(line.strip() for line in open(KEYWORD_FILE))
+undocumented_kws = []
+
+for filename in BIB_FILENAMES:
+    with open(filename, 'r') as bibfile:
+        parser = btp.bparser.BibTexParser()
+        parser.ignore_nonstandard_types = False
+        parser.homogenise_fields = False
+        db = btp.load(bibfile, parser)
+
+        for entry in db.entries:
+            try:
+                keywords = set(kw.strip() 
+                               for kw in entry['keywords'].split(','))
+            except KeyError:
+                continue
+
+            for kw in keywords:
+                if kw not in master_kw_list:
+                    undocumented_kws.append((
+                      kw, entry['ID'], filename))
+
+if undocumented_kws:
+    print("Found undocumented keywords:")
+    for entry in undocumented_kws:
+        print("'{}' in '{}' in '{}'".format(*entry))
+
+    print("Please change these keywords to match similar ones in the master list,")
+    print("or add them if necessary.")
+    sys.exit(1)
